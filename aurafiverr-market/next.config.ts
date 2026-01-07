050f68fcc3f60931cdadf3dabee2a8e081d8cc88
Khang Vu <kangwu123@users.noreply.github.com>
2026-01-07 22:49:58 +0700

    Include UI Section Get Categories Job Base Swagger API

--- aurafiverr-market/next.config.ts
+++ aurafiverr-market/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fiverrnew.cybersoft.edu.vn',
+      },
+      {
+        protocol: 'http',
+        hostname: 'fiverrnew.cybersoft.edu.vn',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
